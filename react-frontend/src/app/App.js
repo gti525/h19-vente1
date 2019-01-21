796ad3c306737fae68d62273488b308c0dc328e8
Philippe Lemoyne <philippe.lemoyne@polymtl.ca>
2019-01-20 19:01:27 -0500

    Initial commit.

--- react-frontend/src/app/App.js
+++ react-frontend/src/app/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Le code de la branche Vincent
+            Le code de la branche Vincent.
           </a>
         </header>
       </div>
