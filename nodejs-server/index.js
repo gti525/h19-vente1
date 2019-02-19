7e94724ace693a305db6a28871b9e028ea439fdd
vpageau <vpageau@dreamsoft.ca>
2019-02-18 21:06:20 -0500

    Fixed package.json

--- nodejs-server/index.js
+++ nodejs-server/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const mongoose = require('mongoose');
+
 const app = express()
-const port = 3000
+const port = 4000
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
