3f6ea3cc2dd6d2450fec840362dc35ef662a0951
vpageau <vincent.pageau.1@gmail.com>
2019-03-15 14:45:57 -0400

    Added payment route

--- nodejs-server/apis/routes.js
+++ nodejs-server/apis/routes.js
@@ -1,1 +1,2 @@
 exports.SOCIAL_API="https://core-api-525.herokuapp.com/api/client";
+exports.PAYMENT_API="https://h19-passerelle.herokuapp.com";
--- nodejs-server/controllers/payment.js
+++ nodejs-server/controllers/payment.js
@@ -1,32 +1,36 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios');
+const { PAYMENT_API } = require("../apis/routes.js");
 
-router.post('/', function(req, res) {
+router.post('/create', function(req, res) {
     var { body } = req;
-    axios.post('ROUTE DE PASSERELLE DE PAIEMENT', {
-       firstName: body.firstName,
-       lastName: body.lastName,
-       cardNumber: body.cardNumber,
-       monthExp: body.monthExp,
-       yearExp: body.yearExp,
-       cvv: body.cvv,
-       amount: body.amount,
-       description: body.description
+    axios.post(`${PAYMENT_API}/transaction/create`, {
+        MERCHANT_API_KEY: "1234567890",
+        amount: 100,
+        purchase_desc: "PURCHASE/ Simons ",
+        credit_card: {
+            first_name: "John",
+            last_name: "Doe",
+            number: 1111222233334444,
+            cvv: 765,
+            exp: {
+            month: 10,
+            year: 2030
+            }
+        },
     })
     .then(function(response) {
         console.log(response);
-        res.status(response.code).json({
-            message: response.message,
-            description: response.description,
-            transactionId: response.transactionId
+        var data = response.data;
+        res.status(200).json({
+            data
         });
     })
     .catch(function(error) {
         console.log(error);
-        res.status(error.code).json({
-            message: error.message,
-            description: error.description
+        res.status(error.response.status).json({
+            message: error.message
         });
     })
 });
