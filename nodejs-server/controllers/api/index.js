0cf79c84b26ce3affaf4fbb049debf3c660132bc
vpageau <vincent.pageau.1@gmail.com>
2019-03-15 15:41:21 -0400

    Updated swagger documentation at /api/swagger

--- nodejs-server/controllers/api/index.js
+++ nodejs-server/controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const path = require('path');
 
 router.use('/swagger', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/swagger.html'));
+    res.sendFile(path.join(__dirname, '../../views/swagger.html'));
 });
 
 router.use('/events', require('./events'));
