c0293c4ecdb4148fdf1f91e8af2f1729903d5e33
k3ssy15 <christelle.buono.1@ens.etsmtl.ca>
2019-03-28 21:39:30 -0400

    ajout maps

--- react-frontend/src/app/reutilisables/map.js
+++ react-frontend/src/app/reutilisables/map.js
@@ -1,21 +1,68 @@
-import React, { Component } from "react";
+//import React, { Component } from "react";
 import { Modal, Button } from "react-bootstrap";
 import './DetailsEvenement.css';
 import { defaultImage } from '../../assistants/images.js';
 import { formatDate } from '../../assistants/dateFormatter.js';
 import GoogleMapReact from 'google-map-react';
- 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+import { GoogleMap, withGoogleMap } from 'react-google-maps';
+import { default as React, Component } from 'react';
+import { Marker} from 'react-google-maps';
+import Geocode from "react-geocode";
 
-class DetailsEvenement extends Component {
+const googleMapUrl = `https://maps.googleapis.com/maps/api/js?key=${'AIzaSyAwK5vgraM6clfN2sin8xyiW7En52KZb0w'}`;
 
-  static defaultProps = {
-    center: {
-      lat: 45.496075,
-      lng: -73.569324
-    },
-    zoom: 17
-  };
+const SimpleGoogleMap = withGoogleMap(props => (
+  <GoogleMap
+    googleMapUrl={googleMapUrl}
+    zoom={props.zoom}
+    center={props.center}
+    >
+    {props.markers.map((marker, index) =>
+      <Marker
+        position={marker.position}
+    ></Marker>)};
+  </GoogleMap>));
+
+const geo = Geocode.setApiKey("AIzaSyAwK5vgraM6clfN2sin8xyiW7En52KZb0w");
+const groupMarkers = [];
+  // set Google Maps Geocoding API for purposes of quota management. Its optional but recommended.
+Geocode.setApiKey("AIzaSyAwK5vgraM6clfN2sin8xyiW7En52KZb0w");
+
+// Enable or disable logs. Its optional.
+Geocode.enableDebug();
+
+// Get address from latidude & longitude.
+Geocode.fromLatLng("48.8583701", "2.2922926").then(
+  response => {
+    const address = response.results[0].formatted_address;
+    console.log(address);
+  },
+  error => {
+    console.error(error);
+  }
+);
+
+// Get latidude & longitude from address.
+Geocode.fromAddress("Eiffel Tower").then(
+  response => {
+    const { lat, lng } = response.results[0].geometry.location;
+    console.log(lat, lng);
+  },
+  error => {
+    console.error(error);
+  }
+);
+
+export class DetailsEvenement extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      zoom: 11,
+      center: { lat: 29.969516, lng: -90.103866 },
+      markers: [],
+      markersLoaded: false,
+    };
+  }
 
   render() {
     const { evenementDetailOuvert, fermerDetailEvenement } = this.props;
@@ -77,20 +124,14 @@ class DetailsEvenement extends Component {
           </Modal.Footer>
 
 
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyAwK5vgraM6clfN2sin8xyiW7En52KZb0w' }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={45.496075}
-            lng={-73.569324}
-            text={'Centre Bell'}
-          />
-        </GoogleMapReact>
-      </div>
 
+          <SimpleGoogleMap containerElement={
+            <div className="mapContainer" />
+            }
+            mapElement={
+              <div className="map" />
+            }
+            />
 
 
 
@@ -99,5 +140,17 @@ class DetailsEvenement extends Component {
      
     );
   } }
-  
+  //for each group location
+  //eslint-disable-next-line
+geo.find(location, (err, res) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const position = res[0].location;
+    groupMarkers.push(position);
+}});
+this.setState({
+  markersLoaded: true,
+});
+
 export default DetailsEvenement;
